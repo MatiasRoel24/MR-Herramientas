14bee5743afdf6f62395bb079f8323051334fdaf
Matias Roel <matiasroel241201@gmail.com>
2022-03-17 14:29:39 -0300

    Carrito primera parte.

--- js/script.js
+++ js/script.js
@@ -8,32 +8,33 @@ let textoColor = "";
 
 /* Objeto de herramientas */
 class Herramienta{
-    constructor(nombre, precio, stock, color,imagen){
+    constructor(nombre, precio, stock, color,imagen,id){
         this.nombre = nombre;
         this.precio = precio;
         this.stock = stock;
         this.color = color;
         this.imagen = imagen;
+        this.id = id;
     }
 }
 
 /* Herramientas */
-const martillo = new Herramienta("Martillo",100,20,"Rojo","../img/martillo.jpg");
-const llaveAlem = new Herramienta("Llave alem",50,10,"Rojo","../img/llave-alem.jpg");
-const pinza = new Herramienta("Pinza",90,40,"Rojo","../img/taladro.jpg");
-const cajaHerramientas = new Herramienta("Caja de Herramientas",500,15,"Azul","../img/caja-de-tornillos.jpg");
-const destornillador = new Herramienta("Destornillador",10,100,"Azul","../img/destornillador.jpg");
+const martillo = new Herramienta("Martillo",100,20,"Rojo","../img/martillo.jpg",1);
+const llaveAlem = new Herramienta("Llave alem",50,10,"Rojo","../img/llave-alem.jpg",2);
+const pinza = new Herramienta("Pinza",90,40,"Rojo","../img/taladro.jpg",3);
+const cajaHerramientas = new Herramienta("Caja de Herramientas",500,15,"Azul","../img/caja-de-tornillos.jpg",4);
+const destornillador = new Herramienta("Destornillador",10,100,"Azul","../img/destornillador.jpg",5);
 
 /* Array de objetos */
 const listaHerramientas = [martillo, llaveAlem, pinza, cajaHerramientas, destornillador]; 
 
-/* Recorre el array */
+/* Recorre el array */ /* ??????????????????? */
 for (const herramienta of listaHerramientas){
         contador++;
         textoMenu += `${contador}- ${herramienta.nombre}: $${herramienta.precio}\n`;
 }
 
-/* FILTRO SELECT */
+/* ---FILTRO SELECT--- */
 
     /* Genero cards completas */
     for(const producto of listaHerramientas){
@@ -50,13 +51,16 @@ for (const herramienta of listaHerramientas){
                                             </div>
                                             <div class="card__descrip">
                                                 <p>El precio es: $${producto.precio}</p>
-                                                <p>El stock es: $${producto.stock}</p>
+                                                </div>    
+                                                <p>El color es: ${producto.color}</p>
                                         </div>
-                                        <a class="btn-grad btn-grad--ancho" href="#">Comprar</a>
+                                        <a id="btn-card" class="btn-grad btn-grad--ancho" href="#">Comprar</a>
                                     </div>
                                 </div>`
             contenedorProductos.appendChild(cards);
+            
     }
+
 
     /* Genero cards filtradas */
     function generadorCards(listaColor){
@@ -72,13 +76,14 @@ for (const herramienta of listaHerramientas){
                                         </div>
                                         <div class="card__info">
                                             <div class="card__titulo">
-                                            <p>${producto.nombre}</p>
+                                                <p>${producto.nombre}</p>
                                             </div>
                                             <div class="card__descrip">
                                                 <p>El precio es: $${producto.precio}</p>
-                                                <p>El stock es: $${producto.stock}</p>
+                                            </div>  
+                                                <p>El color es: ${producto.color}</p>
                                         </div>
-                                        <a class="btn-grad btn-grad--ancho" href="#">Comprar</a>
+                                        <a id="btn-card" class="btn-card btn-grad btn-grad--ancho" href="#">Comprar</a>
                                     </div>
                                 </div>`
             contenedorProductos.appendChild(cards);
@@ -104,7 +109,55 @@ for (const herramienta of listaHerramientas){
         }
     }
 
-/* CARRITO */
+/* ---CARRITO--- */
+
+
+function carrito(){
+    /* let cc = 1 */ /* Contador */
+    const cardBtns = document.querySelectorAll('#btn-card');
+    cardBtns.forEach((cardBtn) => {
+        /* Con esto le puedo dar atributo con numero, pero esta bien? */
+        /* cardBtn.setAttribute('marcador',cc)
+        cc++ */
+        cardBtn.addEventListener('click', compraClick)
+    })
+}
+
+function compraClick(e){
+    let button = e.target;
+    let item = button.closest('.card');
+    
+    let itemTitle = item.querySelector(".card__titulo").textContent;
+    let itemPrecioyStock = item.querySelector(".card__descrip").textContent;
+    let itemImg = item.querySelector('.card__imagen').src;
+    anyadirItemCarrito(itemTitle,itemPrecioyStock,itemImg)
+}
+
+let containerCardsCarrito = document.querySelector(".carrito__container");/* VARIBLE GLOBAL */
+
+function anyadirItemCarrito(itemTitle,itemPrecioyStock,itemImg){
+    let rowCardsCarrito =  document.createElement("div");
+    let cardsCarritoContenido = `
+        <div class="carrito__elementos">
+            <img class="carrito__img" src="${itemImg}" alt="">
+            <h4 class= "carrito__title">${itemTitle}</h4>
+            <button id="boton-agregar" class="btn-agregar">+</button>
+            <h5 class= "carrito__descrip">${itemPrecioyStock}</h5>
+            <button id="boton-vaciar" class="btn-vaciar">X</button>
+            </div>`
+        rowCardsCarrito.innerHTML = cardsCarritoContenido;
+        containerCardsCarrito.append(rowCardsCarrito)
+}
+
+carrito();
+
+
+
+
+
+
+
+
 
 
     
